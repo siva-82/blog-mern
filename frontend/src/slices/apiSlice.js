ca20e45e6186349f504cde3a459cba76f7b2f207
siva-82 <118682661+siva-82@users.noreply.github.com>
2024-05-21 22:27:09 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,9 +1,6 @@
 import  { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
-const baseQuery =fetchBaseQuery({baseUrl:'https://blog-mern-rtk.onrender.com',  prepareHeaders(headers) {
-      return headers;
-    },
-    credentials: "include"})
+const baseQuery =fetchBaseQuery({baseUrl:''})
 
 export const apiSlice=createApi({
     baseQuery,
