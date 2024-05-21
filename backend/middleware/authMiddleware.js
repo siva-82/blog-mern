f75013e04f4c0e9b15d58fd81cc3c5707aa10935
siva-82 <118682661+siva-82@users.noreply.github.com>
2024-05-21 19:25:17 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,9 +3,9 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token;
+    // let token;
 
-    token=req?.cookies.jwt;
+   const token=req?.cookies?.jwt;
 
     if(token){
         try {
