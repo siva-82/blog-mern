6e82ad8269fea91c09dae5680d0683e9bb512f72
siva-82 <118682661+siva-82@users.noreply.github.com>
2024-05-22 17:48:46 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,10 +4,8 @@ const generateToken=(res,userId) =>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:'30d'
     })
-            console.log(token)
-            console.log(process.env.JWT_SECRET)
-    
 
+    
     res.cookie('jwt', token, {
         httpOnly:true,
         secure:process.env.NODE_ENV !== 'development',
@@ -15,7 +13,7 @@ const generateToken=(res,userId) =>{
         sameSite:'none',
         maxAge:30 * 24 * 60* 60 * 1000
     })
-    console.log("console res.cookie",res.cookie,res.jwt)
+   
 }
 
 export default generateToken;
