50bbd040cade198fd54c370825a273dfa9eb3f89
siva-82 <118682661+siva-82@users.noreply.github.com>
2024-05-22 17:47:59 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,33 +3,24 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-     
-    console.log("auth middleware req.body",req.body)
-    console.log("auth middleware req.cookie",req.cookies)
-    console.log("auth middleware req.cookie.jwt",req.cookies)
-    
     let token;
-
     token=req?.cookies?.jwt;
-    console.log("token middleware",token)
+   
     if(token){
         try {
             const decoded = jwt.verify(token,process.env.JWT_SECRET)
-            console.log(process.env.JWT_SECRET)
-            
+                 
             req.user = await User.findById(decoded.userId).select('-password')
-            console.log(req.user)
             
             next();
+             
         } catch (error) {
-            console.log(process.env.JWT_SECRET)
+            
             res.status(401)
             throw new Error('Not Not authorized, invalid token')
         }
 
-    }else{
-    console.log("auth middleware else",req.user)
-         
+    }else{ 
         res.status(401);
         throw new Error('Not authorized, no token')
     }
